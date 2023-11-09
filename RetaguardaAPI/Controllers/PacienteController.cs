fdb56db97b934254dde25464d8049b9e224604e8
Nathan Silva <nathan.osilva1999@hotmail.com>
2023-11-09 00:09:07 -0300

    Adicionar arquivos de projeto.

---
+++ RetaguardaAPI/Controllers/PacienteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RetaguardaAPI.Controllers
+{
+    [Route("api/paciente")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+    }
+}
---
+++ RetaguardaAPI/Entity/Paciente.cs
@@ -0,0 +1,21 @@
+﻿namespace RetaguardaAPI.Entity;
+
+public class Paciente
+{
+    public Guid Ide { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateRegister {  get; set; }
+    public int Status { get; set; }
+
+    public Paciente()
+    {
+        Ide = Guid.Empty;
+        Id = 0;
+        Name = string.Empty;
+        Description = string.Empty;
+        DateRegister = new DateTime(1900, 1, 1);
+        Status = 0;
+    }
+}
